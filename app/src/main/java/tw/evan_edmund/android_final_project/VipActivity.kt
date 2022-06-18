e9080306e80a339774c4474ad03da25a87687393
evan20010126 <55246608+evan20010126@users.noreply.github.com>
2022-06-18 18:28:01 +0800

    activity

---
+++ app/src/main/java/tw/evan_edmund/android_final_project/VipActivity.kt
@@ -0,0 +1,11 @@
+package tw.evan_edmund.android_final_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VipActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vip)
+    }
+}
