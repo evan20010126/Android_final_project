087f65bfc782c87cabce5826f878de0fa1454b45
evan20010126 <55246608+evan20010126@users.noreply.github.com>
2022-06-18 16:55:08 +0800

    fighting

---
+++ app/src/main/java/tw/evan_edmund/android_final_project/FightingActivity.kt
@@ -0,0 +1,11 @@
+package tw.evan_edmund.android_final_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FightingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fighting)
+    }
+}
